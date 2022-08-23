d4a1951a983df46cb8574ecb89f298f5cc4f7a83
Joel Blomberg <joel.blomberg@iths.se>
2022-08-23 14:18:23 +0200

    ...

--- slask/Program.cs
+++ slask/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Linq;
+
+Console.WriteLine("Mini Upg 1");
+for(int i = 9; i > 0; i--)
+{
+    Console.Write(i);
+}
+
+Console.WriteLine();
+Console.WriteLine(string.Concat(Enumerable.Range(1,9).Reverse()));
+
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Mini Upg 2");
+Console.Write("Name, please: ");
+string name = Console.ReadLine();
+
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine(name);
+}
