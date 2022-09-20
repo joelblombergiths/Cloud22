534c5315294271374906d6d81f0322f4bcc6a704
Joel Blomberg <joel.blomberg@iths.se>
2022-09-20 12:04:33 +0200

    ...

--- HangMan/Gallow.cs
+++ HangMan/Gallow.cs
@@ -17,7 +17,18 @@ namespace HangMan
 {
     internal class Gallow
     {
-        private static readonly List<Part> parts = new() { new Hill(), new VerticalBeam(), new HorizontalBeam(), new Rope(), new Head(), new Body(), new LeftArm(), new RightArm(), new LeftLeg(), new RightLeg() };
+        private static readonly List<Part> parts = new() { 
+            new Hill(),
+            new VerticalBeam(),
+            new HorizontalBeam(),
+            new Rope(),
+            new Head(),
+            new Body(),
+            new LeftArm(),
+            new RightArm(),
+            new LeftLeg(),
+            new RightLeg()
+        };
         
         private int stage = 0;
         public bool IsGameOver => stage == parts.Count;
