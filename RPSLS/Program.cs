3796746c4283200ea1bf291f7c1783dc9c036a5c
Joel Blomberg <joel.blomberg@iths.se>
2022-09-22 02:27:45 +0200

    ..

--- RPSLS/Program.cs
+++ RPSLS/Program.cs
@@ -1,34 +1,34 @@
 ﻿using RPSLS;
 using static RPSLS.GameMaster;
 
+Player p0 = Player.CreatePlayer(0);
 Player p1 = Player.CreatePlayer(1);
-Player p2 = Player.CreatePlayer(2);
 
 Console.Clear();
 
+p0.Score = 0;
 p1.Score = 0;
-p2.Score = 0;
 
 do
 {
-    Choice p1Choice = p1.GetChoice();
-    Choice p2Choice = p2.GetChoice();
+    Choice p1Choice = p0.GetChoice();
+    Choice p2Choice = p1.GetChoice();
 
     Console.SetCursorPosition(0, 2);
-    Console.WriteLine($"{p1} chose {p1Choice}".PadRight(50, ' '));
-    Console.WriteLine($"{p2} chose {p2Choice}".PadRight(50, ' '));
+    Console.WriteLine($"{p0} chose {p1Choice}".PadRight(50, ' '));
+    Console.WriteLine($"{p1} chose {p2Choice}".PadRight(50, ' '));
 
     Result result = CheckResult(p1Choice, p2Choice, out string reason);
 
     if (result.Equals(Result.Win))
     {
-        Console.WriteLine($"{reason}. {p1} wins.".PadRight(50, ' '));
-        p1.Score++;
+        Console.WriteLine($"{reason}. {p0} wins.".PadRight(50, ' '));
+        p0.Score++;
     }
     else if (result.Equals(Result.Lose))
     {
-        Console.WriteLine($"{reason}. {p2} wins.".PadRight(50, ' '));
-        p2.Score++;
+        Console.WriteLine($"{reason}. {p1} wins.".PadRight(50, ' '));
+        p1.Score++;
     }
     else
     {
